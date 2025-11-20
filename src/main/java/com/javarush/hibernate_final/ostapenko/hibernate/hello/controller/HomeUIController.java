5d25da0f786e1882d15bd01013544a7be4d6382f
Darfresor <vectnik@gmail.com>
2025-11-20 13:10:33 +0300

    добавлен пример работы с thymleaf

---
+++ src/main/java/com/javarush/hibernate_final/ostapenko/hibernate/hello/controller/HomeUIController.java
@@ -0,0 +1,28 @@
+package com.javarush.hibernate_final.ostapenko.hibernate.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeUIController {
+
+   @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("appTitle", "Мое крутое приложение");
+        model.addAttribute("welcomeMessage", "Добро пожаловать в нашу систему!");
+        model.addAttribute("appName", "quest");
+        model.addAttribute("isAdmin", true);
+        model.addAttribute("userId", 12345);
+        model.addAttribute("infoLinkText", "Подробная информация");
+
+        // Список функций
+        model.addAttribute("features", java.util.Arrays.asList(
+                "Управление задачами",
+                "Отчеты и аналитика",
+                "Командная работа"
+        ));
+
+        return "index"; // Ищет src/main/resources/templates/index.html
+    }
+}
