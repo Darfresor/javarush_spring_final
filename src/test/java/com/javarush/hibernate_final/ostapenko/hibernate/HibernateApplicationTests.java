0b16679bfed549e294201db3d2f8a8052e971326
Darfresor <vectnik@gmail.com>
2025-11-20 12:36:29 +0300

    переключено на spring и добавлен swagger

---
+++ src/test/java/com/javarush/hibernate_final/ostapenko/hibernate/HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javarush.hibernate_final.ostapenko.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
