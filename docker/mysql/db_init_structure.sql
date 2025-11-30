6c41da19cd2e2ac45414d16f0b1df5f312027947
Darfresor <vectnik@gmail.com>
2025-11-30 18:03:34 +0300

    расширены структура бд и добавлены картинки в папку статика для квеста1

--- docker/mysql/db_init_structure.sql
+++ docker/mysql/db_init_structure.sql
@@ -2,6 +2,12 @@ USE myapp;
 
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS quests;
+DROP TABLE IF EXISTS stages;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS link_stage_question;
+DROP TABLE IF EXISTS link_question_answer;
+
 
 CREATE TABLE users
 (
@@ -22,9 +28,46 @@ CREATE TABLE users
 CREATE TABLE quests
 (
     id              BIGINT          NOT NULL    AUTO_INCREMENT PRIMARY KEY,
-    quest_name      VARCHAR(32)     NOT NULL,
-    description     VARCHAR(255)    NOT NULL,
+    quest_name      VARCHAR(100)     NOT NULL,
+    description     VARCHAR(8000)    NOT NULL,
     img_path        VARCHAR(255)    NULL,
     is_new          TINYINT(1)      NOT NULL    DEFAULT 0,
     dt_add          timestamp       NOT NULL    DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE stages
+(
+    id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    quests_id       BIGINT          NOT NULL,
+    description     VARCHAR(8000)    NOT NULL,
+    img_path        VARCHAR(255)    NULL
+);
+
+CREATE TABLE questions
+(
+    id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    quests_id       BIGINT          NOT NULL,
+    description     VARCHAR(255)    NOT NULL
+);
+
+CREATE TABLE answers
+(
+    id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    quests_id       BIGINT          NOT NULL,
+    description     VARCHAR(255)    NOT NULL
+);
+
+CREATE TABLE link_stage_question
+(
+    id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    stages_id       BIGINT          NOT NULL,
+    questions_id    BIGINT          NOT NULL
+);
+
+CREATE TABLE link_question_answer
+(
+    id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    questions_id    BIGINT          NOT NULL,
+    answers_id      BIGINT          NOT NULL
 );
