0b49fb74fdab9333bc846e12e224284ee319ba2c
Darfresor <vectnik@gmail.com>
2025-12-03 19:35:14 +0300

    обновлена структура таблиц и добавлены внешние ключи

--- docker/mysql/db_init_structure.sql
+++ docker/mysql/db_init_structure.sql
@@ -4,9 +4,8 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS quests;
 DROP TABLE IF EXISTS stages;
 DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS quest_tree;
 DROP TABLE IF EXISTS answers;
-DROP TABLE IF EXISTS link_stage_question;
-DROP TABLE IF EXISTS link_question_answer;
 
 
 CREATE TABLE users
@@ -28,8 +27,8 @@ CREATE TABLE users
 CREATE TABLE quests
 (
     id              BIGINT          NOT NULL    AUTO_INCREMENT PRIMARY KEY,
-    quest_name      VARCHAR(100)     NOT NULL,
-    description     VARCHAR(8000)    NOT NULL,
+    quest_name      VARCHAR(100)    NOT NULL,
+    description     VARCHAR(8000)   NOT NULL,
     img_path        VARCHAR(255)    NULL,
     is_new          TINYINT(1)      NOT NULL    DEFAULT 0,
     dt_add          timestamp       NOT NULL    DEFAULT CURRENT_TIMESTAMP
@@ -40,34 +39,41 @@ CREATE TABLE stages
 (
     id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
     quests_id       BIGINT          NOT NULL,
-    description     VARCHAR(8000)    NOT NULL,
-    img_path        VARCHAR(255)    NULL
+    title           VARCHAR(255)    NOT NULL,
+    description     VARCHAR(8000)   NOT NULL,
+    img_path        VARCHAR(255)    NULL,
+    CONSTRAINT fk_stages_quests
+        FOREIGN KEY (quests_id)
+            REFERENCES quests(id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 );
+
 
 CREATE TABLE questions
 (
     id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    quests_id       BIGINT          NOT NULL,
-    description     VARCHAR(255)    NOT NULL
+    stage_id        BIGINT          NOT NULL,
+    description     VARCHAR(255)    NOT NULL,
+    CONSTRAINT uk_questions_stage_id
+        UNIQUE (stage_id),
+    CONSTRAINT fk_questions_stages
+        FOREIGN KEY (stage_id)
+            REFERENCES stages(id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 );
 
 CREATE TABLE answers
 (
     id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    quests_id       BIGINT          NOT NULL,
-    description     VARCHAR(255)    NOT NULL
+    question_id     BIGINT          NOT NULL,
+    next_stage_id   BIGINT          NULL,
+    description     VARCHAR(255)    NOT NULL,
+    CONSTRAINT fk_answers_questions
+        FOREIGN KEY (question_id)
+            REFERENCES questions(id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 );
 
-CREATE TABLE link_stage_question
-(
-    id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    stages_id       BIGINT          NOT NULL,
-    questions_id    BIGINT          NOT NULL
-);
-
-CREATE TABLE link_question_answer
-(
-    id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    questions_id    BIGINT          NOT NULL,
-    answers_id      BIGINT          NOT NULL
-);
