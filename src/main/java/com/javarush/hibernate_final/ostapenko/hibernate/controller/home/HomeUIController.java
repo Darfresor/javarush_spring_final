68e431b33c2d09b1234d6563b17469f8d222f761
Darfresor <vectnik@gmail.com>
2026-01-24 12:33:21 +0300

    добавлено логирование

--- src/main/java/com/javarush/hibernate_final/ostapenko/hibernate/controller/home/HomeUIController.java
+++ src/main/java/com/javarush/hibernate_final/ostapenko/hibernate/controller/home/HomeUIController.java
@@ -1,15 +1,20 @@
 package com.javarush.hibernate_final.ostapenko.hibernate.controller.home;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeUIController {
+    private static final Logger log = LoggerFactory.getLogger(HomeUIController.class);
 
    @GetMapping({"/", "ui/home"})
    public String home(Model model) {
+       log.info("Начало работы контроллера home: {}", model);
+
         model.addAttribute("appTitle", "Мое крутое приложение");
         model.addAttribute("welcomeMessage", "Добро пожаловать в нашу систему!");
         model.addAttribute("appName", "quest");
@@ -23,7 +28,7 @@ public class HomeUIController {
                 "Отчеты и аналитика",
                 "Командная работа"
         ));
-
+        log.info("Завершение работы контроллера home: {}", model);
         return "pages/home"; // Ищет src/main/resources/templates/index.html
     }
 
