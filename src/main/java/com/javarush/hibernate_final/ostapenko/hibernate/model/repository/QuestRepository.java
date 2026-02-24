3d83f210f48a161f09f481c2b38e62d29ec3ce46
Darfresor <vectnik@gmail.com>
2026-02-24 13:24:46 +0300

    убраны ненужные переопределения из репозитория

--- src/main/java/com/javarush/hibernate_final/ostapenko/hibernate/model/repository/QuestRepository.java
+++ src/main/java/com/javarush/hibernate_final/ostapenko/hibernate/model/repository/QuestRepository.java
@@ -3,6 +3,7 @@ package com.javarush.hibernate_final.ostapenko.hibernate.model.repository;
 import com.javarush.hibernate_final.ostapenko.hibernate.model.entity.Quest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,13 +11,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface QuestRepository extends JpaRepository<Quest, Long> {
-    Page<Quest> findAll(Pageable pageable);
-
-    Optional<Quest> findById(Long id);
-
     @Query("SELECT  q FROM Quest q LEFT JOIN FETCH q.stageList WHERE q.id = :questId")
     Optional<Quest> findByIdWithRootStage(Long id);
 
     @Query("SELECT  q FROM Quest q LEFT JOIN FETCH q.stageList WHERE q.id = :questId")
     Optional<Quest> findByIdWithStages(Long questId);
+
 }
