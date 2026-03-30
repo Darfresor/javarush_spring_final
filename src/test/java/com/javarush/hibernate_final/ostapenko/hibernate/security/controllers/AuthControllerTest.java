fb41c220e72974ec05ac1c8bb96ec293c7254252
Darfresor <vectnik@gmail.com>
2026-03-30 21:16:27 +0300

    тест для проверки правильности генерации токена и параметров внутри контроллера

--- src/test/java/com/javarush/hibernate_final/ostapenko/hibernate/security/controllers/AuthControllerTest.java
+++ src/test/java/com/javarush/hibernate_final/ostapenko/hibernate/security/controllers/AuthControllerTest.java
@@ -8,6 +8,7 @@ import com.javarush.hibernate_final.ostapenko.hibernate.security.jwt.JwtService;
 import com.javarush.hibernate_final.ostapenko.hibernate.service.QuestService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -22,8 +23,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -58,6 +60,8 @@ public class AuthControllerTest {
         //настраиваем генерацию токена
         when(jwtService.generateToken(userDetails)).thenReturn("test.jwt.token");
 
+
+
         mockMvc.perform(post("/api/auth/login")
                         .contentType("application/json")
                         .content(objectMapper.writeValueAsString(authRequest))
@@ -81,6 +85,48 @@ public class AuthControllerTest {
                         .content(objectMapper.writeValueAsString(authRequest))
                 ).andExpect(status().isUnauthorized())
                 .andExpect(content().string("Invalid credentials"));
+    }
+
+    @Test
+    void createAuthToken_whenValidInput_shouldAuthenticateWithCorrectCredentials() throws Exception {
+        AuthRequest authRequest = new AuthRequest();
+        authRequest.setUsername("admin");
+        authRequest.setPassword("admin123");
+
+        UserDetails userDetails = new User("admin", "admin123", new ArrayList<>());
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        //при любом вызове аутентификации возвращаем успех
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(authentication);
+        //настраиваем генерацию токена
+        when(jwtService.generateToken(userDetails)).thenReturn("test.jwt.token");
+
+
+
+        mockMvc.perform(post("/api/auth/login")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(authRequest))
+                ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").value("test.jwt.token"));
+
+        // Создаем ArgumentCaptor для проверки аргументов переданных в моки
+        ArgumentCaptor<UsernamePasswordAuthenticationToken> argumentCaptor =
+                ArgumentCaptor.forClass(UsernamePasswordAuthenticationToken.class);
+        // Проверяем, что authenticate был вызван ровно 1 раз
+        verify(authenticationManager, times(1))
+                .authenticate(argumentCaptor.capture());
+
+        // Проверяем, что в authenticate были переданы правильные credentials
+        UsernamePasswordAuthenticationToken capturedArgument = argumentCaptor.getValue();
+        assertEquals("admin", capturedArgument.getPrincipal());
+        assertEquals("admin123", capturedArgument.getCredentials());
+
+        // Проверяем, что generateToken был вызван ровно 1 раз с правильным параметром
+        verify(jwtService, times(1))
+                .generateToken(userDetails);
+
+
     }
 
 
